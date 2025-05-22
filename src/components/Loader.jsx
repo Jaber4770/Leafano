2e35e74bd9760fd25b1789ffc6a06438fb37672e
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-05-22 11:09:52 +0200

    trending tips section done

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div className='flex justify-center items-center'> 
+        <div className='flex justify-center items-center my-48'> 
             <span className="loading loading-spinner text-success"></span>
         </div>
     );
