b5c5baa14f9140a2a5b04e661e5f872c0addffde
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-05-23 17:08:28 +0200

    login with google done

--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
 import { auth } from '../../firebase.init';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 
 const AuthProvider = ({ children }) => {
-    
 
-    const createUser = (email,password) => {
+
+    const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const loginUser = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
+
     const signOutUser = () => {
         return signOut(auth);
     }
+
+    const provider = new GoogleAuthProvider();
+    const loginWithGooglePopUp = () => {
+        return signInWithPopup(auth, provider);
+    }
+
+    const signInWithGooglePopUp = () => {
+        return signInWithPopup(auth, provider);
+    }
+
 
 
     const userInfo = {
         loginUser,
         createUser,
-        signOutUser
+        signOutUser,
+        loginWithGooglePopUp,
+        signInWithGooglePopUp
     }
+
     return (
         <AuthContext value={userInfo}>
             {children}
